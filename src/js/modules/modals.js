a6e6aec5e46abcf63d922d1f6f8b397b1a420cd9
Olga Yavorchuk <olyayavorchuk97@gmail.com>
2023-04-28 12:22:57 +0300

    The tab functionality has been implemented

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -40,7 +40,7 @@ const modals = () => {
 
       bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
       bindModal('.phone_link', '.popup', '.popup .popup_close');
-      showModalDyTime('.popup', 3000);
+      // showModalDyTime('.popup', 60000);
 
 };
 export default modals;
